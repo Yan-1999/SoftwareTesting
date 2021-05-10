b7b03d40937a0553608c04adbbac5d46bc310738
Yan-1999 <49827960+Yan-1999@users.noreply.github.com>
2021-05-10 22:39:59 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <ostream>
 
+#include <cgicc/Cgicc.h>
+#include <cgicc/HTTPContentHeader.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/TestResult.h>
 #include <cppunit/TestResultCollector.h>
 #include <cppunit/TestRunner.h>
-#include <cppunit/TextOutputter.h>
+#include <cppunit/XMLOutputter.h>
 
-#include "triangle_test.h"
+#include "triangle/triangle_test.h"
 
 int main(int argc, char* argv[])
 {
+	cgicc::Cgicc cgi;
+	std::stringstream ss;
+	const char* style_str = "<?xml-stylesheet type=\"text/xsl\" href=\"/test.xsl\"?>";
+
 	CppUnit::TestResult tr;
 	CppUnit::TestResultCollector trc;
 	tr.addListener(&trc);
@@ -21,8 +27,13 @@ int main(int argc, char* argv[])
 	runner.addTest(tect_test_suite);
 	runner.run(tr);
 
-	CppUnit::TextOutputter out(&trc, std::cout);
+	CppUnit::XmlOutputter out(&trc, ss);
 	out.write();
+	std::string s = ss.str();
+	size_t index = s.find_last_of('?');
+	s.insert(index + 2, style_str);
+	std::cout << cgicc::HTTPContentHeader("application/xml");
+	std::cout << s << std::flush;
 
 	return !trc.wasSuccessful();
 }
